7b6dcf6c263c00605bb9599a55196f47f79f2e42
wa <15155313869@163.com>
2018-11-13 17:52:19 +0800

    change

---
+++ src/util/util.js
@@ -0,0 +1,10 @@
+
+/**
+ * 生成随机len位数字
+ */
+export const randomLenNum = (len, date) => {
+  let random = ''
+  random = Math.ceil(Math.random() * 100000000000000).toString().substr(0, typeof len === 'number' ? len : 4)
+  if (date) random = random + Date.now()
+  return random
+}
